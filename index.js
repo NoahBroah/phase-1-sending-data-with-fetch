48cc017bfacc4e88f4c1fa0e88258772fcd41e5b
Noah King <noahking123@yahoo.com>
2022-08-16 17:27:44 -0400

    commit tree 801c5ed80eb789a546ee8316587418497fdac1a5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+function submitData(userName, userEmail) {
+    const userInfo = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.append(data.id)
+    })
+    .catch(error => {
+        alert("Bad things!");
+        document.body.append(error.message);
+    })
+}
